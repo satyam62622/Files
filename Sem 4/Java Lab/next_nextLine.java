988f3edba71b4f4c5d85276a0e9e1f553ef88c60
Satyam Kumar <100254682+satyam62622@users.noreply.github.com>
2022-03-08 10:31:47 +0530

    Add files via upload

---
+++ Sem 4/Java Lab/next_nextLine.java
@@ -0,0 +1,13 @@
+package program.java;
+import java.util.Scanner;
+public class next_nextLine {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Input two strings: ");
+String t=sc.next(); //for only a string
+String t1=sc.nextLine(); //for inputting set of strings with new line
+System.out.println(t);
+System.out.println(t1);
+}
+}	
+//Scanner sc = new Scanner(System.in);
