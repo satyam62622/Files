4c015c0dad45bcc1b0b93bb6471ebb9f5dc4a489
Satyam Kumar <100254682+satyam62622@users.noreply.github.com>
2022-03-22 09:39:34 +0530

    Add files via upload

---
+++ Sem 4/Java Lab/Armstrong.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Armstrong {
+public static void main(String[] Args) {
+	Scanner sc=new Scanner(System.in);
+	int num,result=0,temp,no_of_digits=0,d;
+	System.out.println("Please input any number to check whether it is an Armstrong: ");
+	num=sc.nextInt();
+	temp=num;
+	while(temp!=0)
+	{
+		no_of_digits++;
+		temp=temp/10;
+	}
+	temp=num;
+	while(temp!=0)
+	{
+		d=temp%10;
+		result=result+(int)Math.pow(d,no_of_digits);
+		temp=temp/10;
+	}
+	if(result==num)
+		System.out.println("Number is Armstrong.");
+	else
+		System.out.println("Number is NOT Armstrong.");
+}
+}
+
