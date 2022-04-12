255e25c34b1d05007c73c5e66ca49e47c4c557aa
Satyam Kumar <100254682+satyam62622@users.noreply.github.com>
2022-04-12 09:56:40 +0530

    Add files via upload

---
+++ Sem 4/Java Lab/SmallestLargestArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SmallestLargestArray {
+	public static void main(String[] Args) {
+		
+Scanner sc=new Scanner(System.in);
+System.out.println("Please input array limit: ");
+int n;
+n=sc.nextInt();
+int Arr[]=new int[n];
+System.out.println("Please input array elements: ");
+for(int i=0;i<n;i++)
+	Arr[i]=sc.nextInt();
+FindNums obj=new FindNums();
+obj.findSL(Arr,n);
+}
+	
+}
+
+class FindNums{
+	void findSL(int [] Arr,int n)
+	{
+		int small=Arr[0],large=Arr[0];
+		for(int i=1;i<n;i++)
+		{if(Arr[i]<small)
+				small=Arr[i];
+			if(Arr[i]>large)
+				large=Arr[i];
+			}
+		System.out.println("Smallest: "+small+" Largest: "+large);
+	}
+}
